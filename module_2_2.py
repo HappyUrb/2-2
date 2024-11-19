3edeeae56f4b655efd2b4cee16a7daa7cffc6b1b
HappyUrb <nik.skutin205@gmail.com>
2024-11-19 22:13:41 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -2,7 +2,7 @@ first = (input('Первое число: '))
 second = (input('Второе число: '))
 third = (input('Третье число: '))
 if first == second == third:
-    print(first, second, third, sep='\n')
+    print(3)
 elif first == second or first == third or second == third:
     print(2)
 elif not(first == second == third):
